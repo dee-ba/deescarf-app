bb811f0ac027412e035406765af9d6115843280a
dee-ba <info.dibaalizai@gmail.com>
2022-04-02 11:21:16 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
+import { FooterComponent } from './MyComponents/footer/footer.component';
+import { IntroComponent } from './MyComponents/intro/intro.component';
+import { GalleryComponent } from './MyComponents/gallery/gallery.component';
+import { InfoComponent } from './MyComponents/info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IntroComponent,
+    GalleryComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
